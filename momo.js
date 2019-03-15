7ebd58d857a328998e4044215383f2e539b120e1
kstd2016 <48593363+kstd2016@users.noreply.github.com>
2019-03-15 22:09:21 +0800

    Create momo.js

---
+++ momo.js
@@ -0,0 +1,10 @@
+function 导入常用函数模块() {
+    var url = 'https://raw.githubusercontent.com/snailuncle/autojsCommonFunctions/master/autojsCommonFunctions.js'
+    var r = http.get(url)
+    log("code = " + r.statusCode);
+    var html = r.body.bytes()
+    files.writeBytes('./autojsCommonFunctions.js', html)
+    var common = require('./autojsCommonFunctions.js')
+    return common
+}
+var common = 导入常用函数模块()
